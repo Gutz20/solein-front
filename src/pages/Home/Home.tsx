c8c32e0e76cf9cb0dd782c71b4a3e0d36163bc03
Joe <darckProyect8@gmail.com>
2023-08-30 01:21:05 -0500

    Init

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-9xl font-bold">Ya trabaje 5 minutos es mi descanso de 2 horas jeje (Ahí vengo :v)</h1>
+    </div>
+  );
+};
+
+export default Home;
